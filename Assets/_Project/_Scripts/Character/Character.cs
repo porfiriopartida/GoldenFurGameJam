5138e1946229e3e2bb7fade3bcb331d621f82b78
Porfirio Partida <porfirio.partidai@gmail.com>
2023-04-26 19:28:01 -0700

    Scene setup assembly namespace, initialize character base

--- Assets/_Project/_Scripts/Character/Character.cs
+++ Assets/_Project/_Scripts/Character/Character.cs
@@ -2,7 +2,20 @@ using UnityEngine;
 
 namespace GoldenFur.Character
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class Character : MonoBehaviour
     {
+        public Vector3 direction;
+        private Rigidbody _rigidbody;
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            
+        }
+
+        private void Update()
+        {
+            _rigidbody.velocity = direction;
+        }
     }
 }
