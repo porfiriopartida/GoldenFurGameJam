e639e60cca183b52fc74738bd6daf48facdbf092
Porfirio Partida <porfirio.partidai@gmail.com>
2023-04-27 21:48:09 -0700

    Fix menu clothing

--- Assets/_Project/_Scripts/clothes/NoDestruir.cs
+++ Assets/_Project/_Scripts/clothes/NoDestruir.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NoDestruir : MonoBehaviour
-{
-   
-    private void Awake()
-    {
-        var noDestruirEntreEscenas = FindObjectsOfType<NoDestruir>();
-        if(noDestruirEntreEscenas.Length > 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        DontDestroyOnLoad(gameObject);
-
-    }
-
-    void Start()
-    {
-
-    }
-}
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class NoDestruir : MonoBehaviour
+// {
+//    
+//     private void Awake()
+//     {
+//         var noDestruirEntreEscenas = FindObjectsOfType<NoDestruir>();
+//         if(noDestruirEntreEscenas.Length > 1)
+//         {
+//             Destroy(gameObject);
+//             return;
+//         }
+//
+//         DontDestroyOnLoad(gameObject);
+//
+//     }
+//
+//     void Start()
+//     {
+//
+//     }
+// }
